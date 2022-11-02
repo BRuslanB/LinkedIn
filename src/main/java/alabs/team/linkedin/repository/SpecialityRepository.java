08c7d469a89b4a8e379a6ade8838ed99a9a2eee2
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-11-02 22:47:51 +0600

    third commit

--- src/main/java/alabs/team/linkedin/repository/SpecialityRepository.java
+++ src/main/java/alabs/team/linkedin/repository/SpecialityRepository.java
@@ -2,9 +2,13 @@ package alabs.team.linkedin.repository;
 
 import alabs.team.linkedin.model.Speciality;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Repository
+@Transactional
 public interface SpecialityRepository extends JpaRepository<Speciality, Long> {
     List<Speciality> findAllByOrderByIdAsc();
 }
